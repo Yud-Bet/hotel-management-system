1c7f82bd40b4b0e52caa0297695b2ae8dfc10248
ThanhDan <thanhdancodoc@gmail.com>
2020-12-10 13:14:14 +0700

    connet database with report

---
+++ src/HotelManagement/DataAccess/Report.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace HotelManagement.DataAccess
+{
+    public static class Report
+    {
+        public static DataTable GetRoomRevenue(int RevenueType, DateTime FromDate, DateTime ToDate)
+        {
+            return ExecuteQuery.ExecuteReader("QLKS_GetRoomRevenue @RevenueType , @FromDate , @ToDate",
+                new object[] { RevenueType, FromDate, ToDate });
+        }
+        public static DataTable GetEatServiceRevenue (int RevenueType, DateTime FromDate, DateTime ToDate)
+        {
+            return ExecuteQuery.ExecuteReader("QLKS_GetEatServiceRevenue @RevenueType , @FromDate , @ToDate",
+                new object[] { RevenueType, FromDate, ToDate });
+        }
+        public static DataTable GetLaudryServiceRevenue(int RevenueType, DateTime FromDate, DateTime ToDate)
+        {
+            return ExecuteQuery.ExecuteReader("QLKS_GetLaudryServiceRevenue @RevenueType , @FromDate , @ToDate",
+                new object[] { RevenueType, FromDate, ToDate });
+        }
+    }
+}
